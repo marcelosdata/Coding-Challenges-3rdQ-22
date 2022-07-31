bbde64911599c1bcf3d48d80e420d9d2d8c6f53f
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-07-31 17:57:20 +0000

    Added a function that filter a list of non negative integers and string that returns numbers only. Filter strings

--- index.js
+++ index.js
@@ -251,3 +251,21 @@ class NetflixShows {
 
 let secretVampires = new NetflixShows('Secret Vampires',['Vampires','Terror'],'Robert Pattison','August 24th 2022')
 
+/*
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+//function, return a new list(map), strings filtered(use array method filter condition)
+*/
+
+let filter_list = [1,2,'aasf','1','123',123]
+
+let filter = arr => {
+  let filtro = arr.filter(res =>typeof res === 'number' )
+  return filtro
+}
+
+console.log(filter(filter_list))
