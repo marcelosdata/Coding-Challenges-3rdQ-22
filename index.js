3751d52a1204879799d2f7ded50355926d05ff09
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-08-12 17:33:11 +0000

    Integral number coding challenge

--- index.js
+++ index.js
@@ -463,3 +463,33 @@ function cubeOdd2(arr) {
   arr = arr.reduce((a, b) => a + Math.pow(b, 3), 0);
   return isNaN(arr) ? undefined : arr;
 }
+
+const cubeOdd3 = arr => { 
+
+//Checks if every element in the array doesn't return an undefined value and stores it in a variable
+x = arr.every(y => !isNaN(y)) 
+
+//Using the variable to get the element that is not an undefined value, we check if the array has a string in it or has ONLY numbers
+//and the number should be a defined value, and the string an undefined value, so we check if it's defined and begin our process
+//To remove all even numbers by filtering them and leaving only odd numbers
+//Adding all the odd numbers up and cubing them at the same time
+//And if there was a string in the array we return undefined
+return x ? arr.filter(z => z % 2).reduce((a, b) => a + Math.pow(b, 3), 0) : undefined
+
+}
+
+/*Task
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false*/
+
