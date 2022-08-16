9e143289b4d20105c030b5a17c675270215231b0
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-08-16 17:50:53 +0000

    Added return length of shortes word coding challenge

--- index.js
+++ index.js
@@ -570,3 +570,18 @@ const friend = friends => {
 const friend2 = friends => friends.filter( a => a.length === 4 )
 
 log(friend(amigos))
+
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+const words = 'bitcoin take over the world maybe who knows perhaps'
+function findShort(s){
+  let arr = s.split(' ').sort((a,b) => a.length - b.length )
+    
+  return arr[0].length
+}
+
+log(findShort(words))
