3b5480fd291492473c86fa5be0304c32b361b450
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-08-22 02:47:23 +0000

    Adding return different number in array coding challenge

--- index.js
+++ index.js
@@ -608,3 +608,26 @@ function minMax(arr){
 }
 
 log(minMax(value))
+
+/*You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3*/
+
+const strayArray = [17, 17, 3, 17, 17, 17, 17]
+
+const findStray = numbers => {
+  //let orden =  arr.sort((a,b)=> a - b).
+  return numbers.find(num => numbers.indexOf(num) === numbers.lastIndexOf(num));
+}
+
+console.log(findStray(strayArray))
+
+//console.log(strayArray.length)
+
+
