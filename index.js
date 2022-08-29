1deaab9d75a1b508226c63a2615a7f52c6416c9b
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-08-29 21:31:54 +0000

    Added counting indistinct duplicates coding challenge

--- index.js
+++ index.js
@@ -675,3 +675,30 @@ Example
 "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
 "aA11" -> 2 # 'a' and '1'
 "ABBA" -> 2 # 'A' and 'B' each occur twice*/
+
+const duplStr = 'aaBbcde' //aabb = 2
+
+let countDupl = string => {
+  // makes an array all lowercase and sorts the array in alpha order for easy comparrison
+    let newString = string.toLowerCase().split('').sort();
+    
+    // this array will house the duplicated values so we can 
+    // get the length of it (or the number of duplicated characters). 
+    let newArray = []
+    
+    // set a loop for the array
+    for(i = 0; i < newString.length; i++){
+       // if the current element equals the following element the push it to the new array AND
+       // ONLY if the new array doesn't already include the current element
+       if(newString[i] === newString[i + 1] && !newArray.includes(newString[i])){
+           // push elements to new array
+           newArray.push(newString[i])
+       }
+    }
+    // return the number of elements in the array to represent the number characters that were duplicated
+    return newArray.length
+}
+
+
+log(countDupl(duplStr))
+
