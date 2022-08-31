632f6c800c2e3017d7a305f2fcdfc6c24c669918
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-08-31 21:12:20 +0000

    Find the odd number in a given array Kyu6

--- index.js
+++ index.js
@@ -735,7 +735,8 @@ const alphabetPosition = str => {
 
 log(alphabetPosition(stringPractice))
 
-/*function alphabetPosition(text) {
+/* STUDY THESE ONES
+function alphabetPosition(text) {
   return text
     .toUpperCase()
     .match(/[a-z]/gi)
@@ -753,3 +754,15 @@ log(alphabetPosition(stringPractice))
   })
   return result.join(' ').replace(/\s+/g, ' ').trim()
 }*/
+
+
+/*Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).*/
