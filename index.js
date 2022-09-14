e43e185789def4f5c0819c154737a69ebbc9d2c4
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-09-14 21:30:41 +0000

    Adding a fishy Math Coding challenge...

--- index.js
+++ index.js
@@ -972,3 +972,21 @@ function abbrevName3(name){
   return abr = abr[0][0] + "." + abr[1][0]
 }
 
+/*Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8*/
+
+function rowSumOddNumbers(n) {
+	return Math.pow(n,3)
+}
+
+
